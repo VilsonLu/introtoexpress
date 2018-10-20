0ae5def0f25905aa8fd7da88564769cae6d07ea7
vilsonluffy <vilson.espayos.lu@gmail.com>
2018-10-21 01:28:30 +0800

    added default route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = new express();
 
 const coursesController = require('./coursesController');
 
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
 app.use('/courses', coursesController);
 
 const PORT = process.env.PORT || 3000
